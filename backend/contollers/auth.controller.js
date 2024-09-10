812d3cc234d9a07599135b361afa8e532b99a566
Mperor <f20202239@hyderabad.bits-pilani.ac.in>
2024-09-10 23:10:26 +0530

    project setup with mongo conn

---
+++ backend/contollers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup =  async (req,res)=>{
+    res.json({
+        data:"Signup reached",
+    });
+};
+
+export const login =  async (req,res)=>{
+    res.json({
+        data:"login reached",
+    });
+};
+
+export const logout =  async (req,res)=>{
+    res.json({
+        data:"logout reached",
+    });
+};
